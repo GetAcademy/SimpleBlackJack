f9f9bcea33422e6bd3ebc46c861e9529b09c9355
GET Academy <terje@kolderup.net>
2021-03-26 09:43:43 +0100

    Initial commit

---
+++ SimpleBlackJack/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlackJack
+{
+    class Card
+    {
+        private int _rank;
+        private Suit _suit;
+        public bool IsFaceCard => _rank >= 11;
+        public bool IsAce => _rank == 1;
+        public int Value => 
+            IsAce ? 11 : 
+            IsFaceCard ? 10 : 
+            _rank;
+
+        public Card(int rank, Suit suit)
+        {
+            _rank = rank;
+            _suit = suit;
+        }
+
+    }
+}
---
+++ SimpleBlackJack/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBlackJack
+{
+    class Deck
+    {
+        private readonly Stack<Card> _cards;
+        private static readonly Random _random = new Random();
+
+        public Deck()
+        {
+            var cards = Enumerable
+                .Range(0, 52)
+                .Select(i => new Card(i % 13, (Suit)(i / 13)))
+                .ToArray();
+            Shuffle(cards);
+            _cards = new Stack<Card>(cards);
+        }
+
+        public Card Draw()
+        {
+            return _cards.Pop();
+        }
+
+        private static void Shuffle(Card[] cards)
+        {
+            var cardsCount = cards.Length;
+            for (var i = 0; i < cardsCount; i++)
+            {
+                var deltaIndex = _random.Next(1, cardsCount);
+                var otherIndex = (i + deltaIndex) % cardsCount;
+                SwapCard(cards, i, otherIndex);
+            }
+        }
+
+        private static void SwapCard(Card[] cards, int indexA, int indexB)
+        {
+            var tmp = cards[indexA];
+            cards[indexA] = cards[indexB];
+            cards[indexB] = tmp;
+        }
+    }
+}
